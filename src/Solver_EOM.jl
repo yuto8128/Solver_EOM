0c9c0bfa0d20b7065eb7a735e2fcdce942b0027b
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-06-13 15:05:37 +0900

    add test

--- src/Solver_EOM.jl
+++ src/Solver_EOM.jl
@@ -1,16 +1,19 @@
 module Solver_EOM
 
-greet() = print("Hello World!")
-
 function timeprop(F::Function, tmax, x0, a0, h)
-    currentF::Float64 = F(x, t)
 
+    t_pre = 0.0
     x_pre = x0
     a_pre = a0
-    t_pre = 0
     while (t_pre < tmax)
         t_new = t_pre + h
+        x_new = x_pre + a_pre           * h
         a_new = a_pre + F(t_pre, x_pre) * h
-        x_new = x_pre + a_pre * h
-
+        
+        t_pre = t_new
+        x_pre = x_new
+        a_pre = a_new
+    end
+    return (x_pre, a_pre)
+end
 end # module Solver_EOM
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,65 @@
 using Test
 import Solver_EOM: timeprop
 
-@testset "timeprop" begin
-    
+@testset "Solver_EOM.jl" begin
+    @testset "Uniform motion" begin
+        F(x, t) = 0.0
+        tmax = 1.0
+        x0 = 0.0
+        a0 = 1.0
+        h = 1e-3
+
+        x_final, a_final = timeprop(F, tmax, x0, a0, h)
+
+        expected_x = x0 + a0 * tmax
+        expected_a = a0
+        @test isapprox(x_final, expected_x, rtol=1e-10)
+        @test isapprox(a_final, expected_a, rtol=1e-10)
+    end
+
+    @testset "Uniform acceleration motion" begin
+        F(t, x) = 1.0
+        tmax = 1.0
+        x0 = 0.0
+        a0 = 0.0
+        h = 1e-4
+
+        x_final, a_final = timeprop(F, tmax, x0, a0, h)
+
+        expected_x = x0 + a0*tmax + tmax^2/2.0
+        expected_a = a0 + tmax
+        @test isapprox(x_final, expected_x, rtol=1e-3)
+        @test isapprox(a_final, expected_a, rtol=1e-3)
+    end
+
+    @testset "Harmonic Vibration" begin
+        F(t, x) = -x
+        tmax = 1.0
+        x0 = 0.0
+        a0 = 1.0
+        h = 1e-6
+        
+        x_final, a_final = timeprop(F, tmax, x0, a0, h)
+        
+        expected_x = sin(tmax)
+        expected_a = cos(tmax)
+        @test isapprox(x_final, expected_x, rtol=1e-5)
+        @test isapprox(a_final, expected_a, rtol=1e-5)
+    end
+
+    @testset "Time Dipendent Force" begin
+        F(t, x) = cos(t)
+        tmax = 1.0
+        x0 = 0.0
+        a0 = 1.0
+        h = 1e-3
+        
+        x_final, a_final = timeprop(F, tmax, x0, a0, h)
+        
+        expected_x = 1.0 + tmax - cos(tmax)
+        expected_a = 1.0 + sin(tmax)
+        @test isapprox(x_final, expected_x, rtol=1e-3)
+        @test isapprox(a_final, expected_a, rtol=1e-3)
+    end
 end
 
