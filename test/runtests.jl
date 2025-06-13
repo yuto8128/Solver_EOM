e48276f43be1133ade187abe493b613ee6bfbb14
euclid8128 <y.kato.534@ms.saitama-u.ac.jp>
2025-06-13 15:23:01 +0900

    change test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 import Solver_EOM: timeprop
 
 @testset "Solver_EOM.jl" begin
-    @testset "Uniform motion" begin
+    @testset "Uniform velocity motion" begin
         F(x, t) = 0.0
         tmax = 1.0
         x0 = 0.0
@@ -32,6 +32,21 @@ import Solver_EOM: timeprop
         @test isapprox(a_final, expected_a, rtol=1e-3)
     end
 
+    @testset "Uniform jerk motion" begin
+        F(t, x) = t
+        tmax = 1.0
+        x0 = 0.0
+        a0 = 0.0
+        h = 1e-4
+        
+        x_final, a_final = timeprop(F, tmax, x0, a0, h)
+        
+        expected_x = tmax^3/6.0
+        expected_a = tmax^2/2.0
+        @test isapprox(x_final, expected_x, rtol=1e-3)
+        @test isapprox(a_final, expected_a, rtol=1e-3)
+    end
+
     @testset "Harmonic Vibration" begin
         F(t, x) = -x
         tmax = 1.0
@@ -47,19 +62,5 @@ import Solver_EOM: timeprop
         @test isapprox(a_final, expected_a, rtol=1e-5)
     end
 
-    @testset "Time Dipendent Force" begin
-        F(t, x) = cos(t)
-        tmax = 1.0
-        x0 = 0.0
-        a0 = 1.0
-        h = 1e-3
-        
-        x_final, a_final = timeprop(F, tmax, x0, a0, h)
-        
-        expected_x = 1.0 + tmax - cos(tmax)
-        expected_a = 1.0 + sin(tmax)
-        @test isapprox(x_final, expected_x, rtol=1e-3)
-        @test isapprox(a_final, expected_a, rtol=1e-3)
-    end
 end
 
